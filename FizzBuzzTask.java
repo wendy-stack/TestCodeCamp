1c74390128a09f4cff84be83d4257454b5448ec5
wendy-stack <sethwendy@yahoo.com>
2021-09-16 22:13:06 +0100

    modified.FizzBuzzTask

--- FizzBuzzTask.java
+++ FizzBuzzTask.java
@@ -4,13 +4,13 @@ public class FizzBuzzTask {
     public static void main(String[] args) {
         int n = 30;
         for (int i=1; i<=n; i++) {
-            if (i%15==0)
+            if (i/15==0)
                 System.out.print("FizzBuzz"+" ");
 
-            else if (i%5==0)
+            else if (i/3==0)
                 System.out.print("Buzz"+" ");
 
-            else if (i%3==0)
+            else if (i/5==0)
                 System.out.print("Fizz"+" ");
             else
                 System.out.print(i+" ");
