7026b3642cdbd059ccc4d2b0bfcbfa84a1973518
wendy-stack <sethwendy@yahoo.com>
2021-09-16 19:09:51 +0100

    BasicArithmeticOps

---
+++ BasicArithmeticOps.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class BasicArithmeticOps {
+    public static void main(String[] args) {
+        //basic arithmetic
+
+        //byte age1 = 20;
+        //byte age2 = 30;
+        double age1 = 20;
+        double age2 = 30;
+
+
+        //System.out.println(age1 + age2);
+        //System.out.println(age1 - age2);
+        //System.out.println(age1 / age2);
+        //System.out.println(age1 * age2);
+
+
+        //Incremental and Decremental ++ --
+        //age1++;
+        //age1--;
+        //System.out.println(age1);
+
+        //compound assignment operators -= += /= *=
+        //age1 -= age2;
+        //System.out.println(age1);
+
+       // age1 += age2;
+        //System.out.println(age1);
+
+        //age1 /= age2;
+        //System.out.println(age1);
+
+        //age1 *= age2;
+        //System.out.println(age1);
+
+
+
+    }
+}
