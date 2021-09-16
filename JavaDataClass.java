fd2e449b96fcc71fb289205fa6d8c593584708d1
wendy-stack <sethwendy@yahoo.com>
2021-09-16 16:37:09 +0100

    Task 14(Print 6 variables using different Data Types)

---
+++ JavaDataClass.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class JavaDataClass {
+    public static void main(String[] args) {
+
+        byte age = 127;
+
+        short money = 32_76;
+
+        long soMuchMoney = -214564753l;
+
+        float area = 1234567.90f;
+
+        double biggerArea = 1234567.67;
+
+        System.out.println(biggerArea+age-money);
+        System.out.println(soMuchMoney);
+        System.out.println(area);
+
+
+        char option = 'm';
+
+        boolean isAround = true;
+
+        System.out.println(isAround);
+
+        System.out.print(option);
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
