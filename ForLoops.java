c2300e6ef1d741f63e5a64c4f6f66d4d019d7b99
wendy-stack <sethwendy@yahoo.com>
2021-09-22 19:06:35 +0100

    Completed_task_21A

---
+++ ForLoops.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class ForLoops {
+    public static void main(String[] args) {
+       // for i loops incrementing
+       // for ( int i = 0; i <= 10; i ++ ) {
+          //  System.out.println( "student: " + i);
+        //}
+
+        // for i loops decrementing
+
+        // for i loops incrementing
+      //  for ( int i = 10; i > 5; i -- ) {
+      //      System.out.println( "student: " + i);
+     //   }
+
+        // for each loops
+
+        String [] fruits = {"mango","pineapple","pear"};
+
+        for (String vName: fruits) {
+            System.out.println(vName);
+        }
+
+
+
+
+    }
+}
---
+++ ForLoopsTask.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class ForLoopsTask {
+    public static void main(String[] args) {
+        int n = 10;
+        for ( int i = 0; i <= 10; i ++ ) {
+            if (i%2 == 0)
+                System.out.println("odd");
+            else
+                System.out.println("even");
+        }
+    }
+}
