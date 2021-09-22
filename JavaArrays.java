f18d08d6778b602a762cabe1788936dcb3ee32cf
wendy-stack <sethwendy@yahoo.com>
2021-09-22 16:45:57 +0100

    Completed_Lesson_19

---
+++ JavaArrays.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class JavaArrays {
+    public static void main(String[] args) {
+        int [] array1 = new int[5];
+        array1 [0] = 10;
+        array1 [1] = 1;
+        array1 [4] = 4;
+        //System.out.println(Arrays.toString(array1));
+
+        String [] Fruits = {"mango", "apple", "guava"};
+        System.out.println(Arrays.toString(Fruits));
+
+        int [] [] doubleDimension = new int [2] [3];
+        //doubleDimension [0] [0] = 20 ;
+        //doubleDimension [0] [1] =  30;
+        //System.out.println(Arrays.deepToString(doubleDimension));
+
+        //ArrayList
+        //ArrayList arr = new ArrayList();
+        //arr.add("test");
+       // arr.add(10);
+        //arr.remove(0);
+        //System.out.println(arr.remove(0));
+
+
+
+
+
+
+    }
+}
---
+++ TaskNineteen.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class JavaArraysAssignment {
+    public static void main(String[] args) {
+        String [] [] fruits = new String[4][3];
+        fruits [0] [0] = "mango";
+        fruits [1] [0] =  "grapes";
+        fruits [2] [0] = "cherries";
+        fruits [3] [0] = "melons";
+        System.out.println(Arrays.deepToString(fruits));
+
+
+
+
+
+
+
+
+
+
+    }
+}
