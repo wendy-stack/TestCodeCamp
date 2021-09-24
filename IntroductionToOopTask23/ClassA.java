371252ac4839c97fca67b8abeaa61d591a2c5ed4
wendy-stack <sethwendy@yahoo.com>
2021-09-24 12:12:08 +0100

    Completed the Oop Task

---
+++ IntroductionToOopTask23/ClassA.java
@@ -0,0 +1,11 @@
+package com.introToOop;
+
+public class ClassA {
+
+    String name;
+
+    public void myName (String yourName){
+        name = yourName;
+        System.out.println(yourName);
+    }
+}
---
+++ IntroductionToOopTask23/ClassB.java
@@ -0,0 +1,10 @@
+package com.introToOop;
+
+public class ClassB {
+    public static void main(String[] args) {
+        ClassA nameVar = new ClassA();
+        nameVar.name = "wendy";
+
+        nameVar.myName(nameVar.name);
+    }
+}
